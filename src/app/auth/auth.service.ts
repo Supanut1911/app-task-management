1387869b68649b7782da9ed45168fe74c7de71e7
Supanut1911 <ynotmela@gmail.com>
2024-01-04 21:30:12 +0700

    test signup and login success to navigate route

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -2,12 +2,16 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { environment } from '../../environments/environment.development';
 import { AuthData } from './auth-data.model';
+import { Router } from '@angular/router';
 const BACKEND_API = environment.BACKEND_URL;
 @Injectable({
   providedIn: 'root',
 })
 export class AuthService {
-  constructor(private readonly http: HttpClient) {}
+  constructor(
+    private readonly http: HttpClient,
+    private readonly router: Router
+  ) {}
 
   createUser(username: string, password: string) {
     const authData: AuthData = {
@@ -18,6 +22,7 @@ export class AuthService {
       .post<AuthData>(BACKEND_API + '/user/signup', authData)
       .subscribe((response) => {
         console.log(response);
+        this.router.navigate(['/login']);
       });
   }
 
@@ -28,6 +33,7 @@ export class AuthService {
     };
     this.http.post(BACKEND_API + '/auth/login', authData).subscribe((res) => {
       console.log(res);
+      this.router.navigate(['/']);
     });
   }
 }
